7156d028bbb78454764ea5cbcb636600f897b05a
Muhammad Yasir <myasir47991@gmail.com>
2024-08-15 18:02:17 +0500

    orders page ui done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -8,6 +8,7 @@ const Login = lazy(() => import("./pages/login"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
+const Orders = lazy(() => import("./pages/orders"));
 
 // Admin Routes
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -44,6 +45,7 @@ const App = () => {
         {/* Logged In User Routes */}
         <Route>
           <Route path="/shipping" element={<Shipping />} />
+          <Route path="/orders" element={<Orders />} />
         </Route>
         
         {/* Admin Routes */}
